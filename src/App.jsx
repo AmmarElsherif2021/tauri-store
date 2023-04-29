16c1277ba8746000cffed5d0a6cf309ad73bf688
Ammar <ammar.alshareef2021@outlook.com>
2023-04-29 17:19:20 +0200

    login issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 //import Hello from "./Hello/Hello";
 import Login from "./Container/Login";
 import "./App.css";
-//simport { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from "./Dashboard/Dashboard";
+import Preferences from "./Praferences/Preferences";
 
 function App() {
-  return (<div className='app'>
-    <Login/>
-  </div>)
+  return (
+    <div className="app">
+      <h1>Application</h1>
+      <Dashboard/>
+      <Preferences/>
+    </div>
+  
+  )
 }
 
 export default App;
